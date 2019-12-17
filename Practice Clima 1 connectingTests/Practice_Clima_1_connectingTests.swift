c735c29bcb7290deeeb9b08bc9e39c7d86738c7b
ufoxcode <ufoxcode@gmail.com>
2019-12-17 11:34:51 +0800

    Initial Commit

---
+++ Practice Clima 1 connectingTests/Practice_Clima_1_connectingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Practice_Clima_1_connectingTests.swift
+//  Practice Clima 1 connectingTests
+//
+//  Created by UFO Xcode on 2019/12/17.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import XCTest
+@testable import Practice_Clima_1_connecting
+
+class Practice_Clima_1_connectingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
